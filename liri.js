f69ce2d30519f88243c11626aaf535ca5820cb89
Etienne Warneck <etienne.warneck@gmail.com>
2019-11-15 11:33:53 -0500

    OMDB and Spotify working

--- liri.js
+++ liri.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
-require('fs');
+var fs = require('fs');
 var keys = require("./keys"); //this is loaded
-var Spotify = require('node-spotify-api'); //
+var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
-var Axios = require('axios'); //
+var Axios = require('axios'); //for OMDB
+// var bandsintown = require('bandsintown')(codingbootcamp);
 
 var user_command = process.argv[2];
 var user_input = process.argv[3];
@@ -13,9 +14,14 @@ switch (user_command) {
         break;
     case 'movie-this': movie_this();
         break;
+    case 'concert-this': concert_this();
+        break;
+    case 'do-what-it-says': do_what_it_says();
+        break;
+    default: "Write spotify_this, movie_this, concert_this or do_what_it_says"
 }
 
-//SPOTIFY-------------------------------------
+//SPOTIFY----------------------------------------------
 function spotify_this() {
     spotify.search({ type: 'track', query: user_input, limit: 1 },
         function (err, data) {
@@ -29,9 +35,10 @@ function spotify_this() {
         });
 };
 
-// //AXIOS for OMDb -------------------------------------
+//AXIOS for OMDb -------------------------------------
 function movie_this() {
-    Axios.get("http://www.omdbapi.com/?t=" + user_input + "&apikey=dbac59f0")
+    Axios
+        .get("http://www.omdbapi.com/?t=" + user_input + "&apikey=dbac59f0")
         .then(function (response) {
             console.log(response.data.Title);//   * Title of the movie.
             console.log(response.data.Year);//   * Year the movie came out.
@@ -54,15 +61,37 @@ function movie_this() {
         });
 };
 
+//BANDS IN TOWN ------------------------------
+// Name of the venue
+// Venue location
+// Date of the Event (use moment to format this as "MM/DD/YYYY")
+// "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
 
-// //BANDS IN TOWN ------------------------------
+// function concert_this() {
+//     Axios
+//         .getArtist("https://rest.bandsintown.com/artists/" + user_input + "/events?app_id=codingbootcamp")
+//         // .getArtistEventList(user_input)
+//         .then(function (events) {
+//             console.log(events.artistName)
+//         })
+// };
 
-// var bandsintown = require('bandsintown')(APP_ID);
+//DO WHAT IT SAYS
+// function do_what_it_says() {
+//     fs.readFile("random.txt", "utf8", function (error, data) {
 
-// bandsintown
-//   .getArtistEventList('')
-//   .then(function(events) {
-//     // return array of events
-//   });
+//         // If the code experiences any errors it will log the error to the console.
+//         if (error) {
+//             console.log(error);
+//         } else {
+//             console.log(data);
+//         };
+//         // // Then split it by commas (to make it more readable)
+//         // var dataArr = data.split(",");
+
+//         // // We will then re-display the content as an array for later use.
+//         // console.log(dataArr);
+//     });
+// };
 
 
